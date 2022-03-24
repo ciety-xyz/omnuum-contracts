cca380bd9d215d4b868308a358507d9794368677
Jaemin Kim <ddevkim@gmail.com>
2022-03-24 22:34:32 +0900

    chore(verification): verify contracts arguments

--- scripts/deployments/constructorArgs/mintManager.js
+++
@@ -1,3 +0,0 @@
-const { testValues } = require('../../../utils/constants');
-
-module.exports = [testValues.baseFeeRate]; // baseFeeRate
---
+++ scripts/verifications/revealManager.js
@@ -0,0 +1,1 @@
+module.exports = ['0x07A717f17cC8C3334BC974c3B902C7e3Ec4a760d']; // ca Manager Proxy Address
