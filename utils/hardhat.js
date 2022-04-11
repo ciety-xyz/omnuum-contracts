af1bb3fdbd469486ed851903a9ab6ac9fa81e093
gyeonghun <erguono@gmail.com>
2022-04-11 18:42:24 +0900

    chore: deployment script update
    - use absolute path

--- utils/hardhat.js
+++ utils/hardhat.js
@@ -1,12 +1,13 @@
 const { ethers, upgrades, config, run } = require('hardhat');
 const { rm } = require('fs/promises');
+const path = require('path');
 const { mapC, go } = require('fxjs');
 
 const clean = async () => {
   const targets = ['artifacts', 'cache', '.openzeppelin', 'data'];
   await go(
     targets,
-    mapC((target) => rm(`./${target}`, { recursive: true, force: true })),
+    mapC((target) => rm(path.join(__dirname, `./${target}`), { recursive: true, force: true })),
   );
 };
 
