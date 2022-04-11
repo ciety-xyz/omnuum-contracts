da90c30db7e05730f974b90d3a8f31977b73899b
gyeonghun <erguono@gmail.com>
2022-04-11 18:04:49 +0900

    chore: deployment script update
    - move hardhat compile from package.json to inline script
    - create hardhat util functions (clean, compile)

---
+++ utils/hardhat.js
@@ -0,0 +1,19 @@
+const { ethers, upgrades, config, run } = require('hardhat');
+const { rm } = require('fs/promises');
+const { mapC, go } = require('fxjs');
+
+const clean = async () => {
+  const targets = ['artifacts', 'cache', '.openzeppelin', 'data'];
+  await go(
+    targets,
+    mapC((target) => rm(`./${target}`, { recursive: true, force: true })),
+  );
+};
+
+module.exports.compile = async ({ quiet, force }) => {
+  if (force) {
+    await clean();
+  }
+
+  await run('comile', { force, quiet });
+};
