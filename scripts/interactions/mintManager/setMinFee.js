9f9f37ccf87a7502b63393e76ba6ed97a36b0b64
gyeonghun <erguono@gmail.com>
2022-05-10 15:07:10 +0900

    feat: create 'update min fee interaction script

---
+++ scripts/interactions/mintManager/setMinFee.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer');
+const { ethers } = require('hardhat');
+const { nullCheck, getRPCProvider } = require('../../deployments/deployHelper');
+const { testValues } = require('../../../utils/constants');
+
+const questions = [
+  {
+    name: 'deployer_private_key',
+    type: 'input',
+    message: '🤔 MintManager deployer private key is ...',
+    validate: nullCheck,
+  },
+  {
+    name: 'mint_manager_address',
+    type: 'input',
+    message: '🤔 MintManager contract address is ...',
+    validate: nullCheck,
+  },
+  {
+    name: 'minFee',
+    type: 'input',
+    message: '🤔 MinFee value ... (in ether)',
+    validate: nullCheck,
+  },
+];
+
+(async () => {
+  inquirer.prompt(questions).then(async (ans) => {
+    try {
+      const provider = await getRPCProvider(ethers.provider);
+      const deployerSigner = new ethers.Wallet(ans.deployer_private_key, provider);
+      const mintManager = (await ethers.getContractFactory('OmnuumMintManager')).attach(ans.mint_manager_address);
+
+      const txResponse = await mintManager.connect(deployerSigner).setMinFee(ethers.utils.parseEther(ans.minFee));
+
+      const txReceipt = await txResponse.wait();
+
+      console.log(txReceipt);
+
+      console.log(`\n\nUpdated minFee: ${await mintManager.minFee()}`);
+    } catch (e) {
+      console.error('\n 🚨 ==== ERROR ==== 🚨 \n', e);
+    }
+  });
+})();
