78f69d993265ff09a627340fedeb3cc50d0e0b84
Jamie <ddevkim@gmail.com>
2022-05-11 18:02:29 +0900

    chore:hardhat config default network change to localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ require('solidity-coverage');
 
 module.exports = {
   solidity: '0.8.10',
-  defaultNetwork: 'rinkeby',
+  defaultNetwork: 'localhost',
   networks: {
     hardhat: {
       accounts: {
