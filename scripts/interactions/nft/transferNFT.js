6a64c5a58138a3f680cf2eb4ab6c59ab6c0a8783
Jamie <ddevkim@gmail.com>
2022-05-20 19:51:49 +0900

    fix:nft transfer

--- scripts/interactions/nft/transferNFT.js
+++ scripts/interactions/nft/transferNFT.js
@@ -2,26 +2,6 @@ const { ethers } = require('hardhat');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const { nullCheck, getRPCProvider } = require('../../deployments/deployHelper');
-// method: safeTransferFrom
-// signature: safeTransferFrom(
-//         address from,
-//         address to,
-//         uint256 id,
-//         uint256 amount,
-//         bytes memory data
-//     )
-//  emit TransferSingle(operator, from, to, id, amount);
-// from
-// to
-// tokenId
-// amount = 1
-// date = ""
-
-// test
-// nft_contract: 0x681d4eeadaa2817715704ba7e28b3640a1fb626d
-// from private key : 13383353df61dad1bd645eefd49548e22153423055c7b0089a8ab772077ffda2 (TESTER_E)
-// to address : 0xF891E5556686b588269762d59466451FD7cE49B9
-// token_id : 1
 
 const inquirerParams = {
   nftContractAddress: 'nftContractAddress',
@@ -64,48 +44,53 @@ const questions = [
 
       const nftContract = (await ethers.getContractFactory('OmnuumNFT721')).attach(ans.nftContractAddress).connect(nftOwnerSigner);
 
-      const txResponseApprove = await nftContract.approve(ans.receiverAddress, ans.tokenId);
-      const txReceiptApprove = await txResponseApprove.wait();
+      const { wantToApprove } = inquirer.prompt({
+        name: 'wantToApprove',
+        type: 'confirm',
+        message: '🤔 Want to approve?',
+      });
 
-      // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-      const { approved } = nftContract.interface.parseLog(txReceiptApprove.events[0]).args;
-
-      if (approved === ans.receiverAddress) {
+      if (wantToApprove) {
+        const txResponseApprove = await nftContract.approve(ans.receiverAddress, ans.tokenId);
+        const txReceiptApprove = await txResponseApprove.wait();
+        // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+        const { approved } = nftContract.interface.parseLog(txReceiptApprove.events[0]).args;
         console.log(`\n${chalk.yellow(`Token id ${ans.tokenId} is ${chalk.redBright('approved')} to receiver ${approved}`)}\n`);
-        const { tokenTrasnfer } = await inquirer.prompt([
-          {
-            name: 'tokenTrasnfer',
-            type: 'list',
-            message: '🤔 Choose transfer method?',
-            choices: ['safeTrasnferFrom', 'transferFrom', 'cancel'],
-          },
-        ]);
-        if (tokenTrasnfer !== 'cancel') {
-          let txResponseSafeTransfer;
-          const transferArguments = [nftOwnerSigner.address, ans.receiverAddress, ans.tokenId];
-          if (tokenTrasnfer === 'safeTrasnferFrom') {
-            txResponseSafeTransfer = await nftContract['safeTransferFrom(address,address,uint256)'](...transferArguments);
-          } else if (tokenTrasnfer === 'transferFrom') {
-            txResponseSafeTransfer = await nftContract.transferFrom(...transferArguments);
-          }
+      }
 
-          const txReceiptSafeTransfer = await txResponseSafeTransfer.wait();
+      const { tokenTrasnfer } = await inquirer.prompt([
+        {
+          name: 'tokenTrasnfer',
+          type: 'list',
+          message: '🤔 Choose transfer method?',
+          choices: ['safeTrasnferFrom', 'transferFrom', 'cancel'],
+        },
+      ]);
+      if (tokenTrasnfer !== 'cancel') {
+        let txResponseSafeTransfer;
+        const transferArguments = [nftOwnerSigner.address, ans.receiverAddress, ans.tokenId];
+        if (tokenTrasnfer === 'safeTrasnferFrom') {
+          txResponseSafeTransfer = await nftContract['safeTransferFrom(address,address,uint256)'](...transferArguments);
+        } else if (tokenTrasnfer === 'transferFrom') {
+          txResponseSafeTransfer = await nftContract.transferFrom(...transferArguments);
+        }
 
-          // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-          // event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-          const { to } = nftContract.interface.parseLog(txReceiptSafeTransfer.events[1]).args;
-          if (to === ans.receiverAddress) {
-            console.log(`\n${chalk.yellow(`Token id ${ans.tokenId} is ${chalk.redBright('safe transferred')} to receiver ${approved}`)}\n`);
-            console.log(
-              `💋 Token ${ans.tokenId} from nftContract ${ans.nftContractAddress} is ${chalk.redBright(
-                'safe transferred',
-              )} to receiver ${approved}.\nBlock: ${txReceiptSafeTransfer.blockNumber}\nTransaction: ${
-                txReceiptSafeTransfer.transactionHash
-              }`,
-            );
-          } else {
-            throw new Error('Wrong receiver');
-          }
+        const txReceiptSafeTransfer = await txResponseSafeTransfer.wait();
+
+        // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+        // event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+        const { to } = nftContract.interface.parseLog(txReceiptSafeTransfer.events[1]).args;
+        if (to === ans.receiverAddress) {
+          console.log(
+            `\n${chalk.yellow(`Token id ${ans.tokenId} is ${chalk.redBright('safe transferred')} to receiver ${ans.receiverAddress}`)}\n`,
+          );
+          console.log(
+            `💋 Token ${ans.tokenId} from nftContract ${ans.nftContractAddress} is ${chalk.redBright('safe transferred')} to receiver ${
+              ans.receiverAddress
+            }.\nBlock: ${txReceiptSafeTransfer.blockNumber}\nTransaction: ${txReceiptSafeTransfer.transactionHash}`,
+          );
+        } else {
+          throw new Error('Wrong receiver');
         }
       }
     } catch (e) {
