473889b65c36fb577369b43a21a0be1ff0b46db4
Jamie <ddevkim@gmail.com>
2022-07-01 12:04:08 +0900

    feat: import ERC721BurnableUpgrade

--- contracts/V1/OmnuumNFT721.sol
+++ contracts/V1/OmnuumNFT721.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.10;
 
 import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
