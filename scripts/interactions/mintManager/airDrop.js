f9d3a06097256755d274f9eb28a75ef4efbd8389
Jamie <ddevkim@gmail.com>
2022-07-04 13:01:39 +0900

    feat: airdrop gas estimation

--- scripts/interactions/mintManager/airDrop.js
+++ scripts/interactions/mintManager/airDrop.js
@@ -11,6 +11,7 @@ const inquirerParams = {
   quantity_to_A: 'quantity_to_A',
   airdrop_receiver_B: 'airdrop_receiver_B',
   quantity_to_B: 'quantity_to_B',
+  estimate_gas: 'estimate_gas',
 };
 
 const questions = [
@@ -35,26 +36,19 @@ const questions = [
   {
     name: inquirerParams.airdrop_receiver_A,
     type: 'input',
-    message: '🤔 Airdrop receiver address A is...',
+    message: '🤔 Airdrop receiver address is...',
     validate: nullCheck,
   },
   {
     name: inquirerParams.quantity_to_A,
     type: 'input',
-    message: '🤔 How many nfts airdrop to receiver A is...',
+    message: '🤔 How many nfts airdrop to receiver is...',
     validate: nullCheck,
   },
   {
-    name: inquirerParams.airdrop_receiver_B,
-    type: 'input',
-    message: '🤔 Airdrop receiver address B is...',
-    validate: nullCheck,
-  },
-  {
-    name: inquirerParams.quantity_to_B,
-    type: 'input',
-    message: '🤔 How many nfts airdrop to receiver B is...',
-    validate: nullCheck,
+    name: inquirerParams.estimate_gas,
+    type: 'confirm',
+    message: '🤔 Just estimate gas?',
   },
 ];
 
@@ -65,15 +59,19 @@ const questions = [
       const nftOwnerSigner = new ethers.Wallet(ans.nft_owner_private_key, provider);
       const mintManager = (await ethers.getContractFactory('OmnuumMintManager')).attach(ans.mint_manager_address);
 
-      const value = testValues.minFee.mul(Number(ans.quantity_to_A) + Number(ans.quantity_to_B));
-      const txResponse = await mintManager
-        .connect(nftOwnerSigner)
-        .mintMultiple(ans.nft_address, [ans.airdrop_receiver_A, ans.airdrop_receiver_B], [ans.quantity_to_A, ans.quantity_to_B], { value });
+      const value = testValues.minFee.mul(Number(ans.quantity_to_A));
+      const args = [ans.nft_address, [ans.airdrop_receiver_A], [ans.quantity_to_A], { value }];
 
+      if (ans.estimate_gas) {
+        const gas = await mintManager.connect(nftOwnerSigner).estimateGas.mintMultiple(...args);
+        console.log(`Gas Estimation: ${gas}`);
+        return;
+      }
+
+      const txResponse = await mintManager.connect(nftOwnerSigner).mintMultiple(...args);
       console.log('txRseponse', txResponse);
 
       const txReceipt = await txResponse.wait();
-
       console.log(txReceipt);
       console.log(`💋 Air drop is on the way.\nBlock: ${txReceipt.blockNumber}\nTransaction: ${txReceipt.transactionHash}`);
     } catch (e) {
